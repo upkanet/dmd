c5c0a36a9b6800ec817115ac92c891f6fe56c2f1
upkanet <jeandamien.louise@gmail.com>
2021-01-28 17:10:01 +0100

    ArrayBuffer received

--- ffi.mjs
+++ ffi.mjs
@@ -2,18 +2,19 @@ import { ALP } from './alp.mjs';
 import http from 'http';
 import open from 'open';
 import fs from 'fs';
-import atob from 'atob';
 
 var alp = new ALP();
 alp.init();
+var data = [];
 
 open('http://localhost:8080/');
 http.createServer(function (req, res) {
     try {
         console.log(req.method);
-        if(req.method === 'GET'){
+        if (req.method === 'GET') {
             if (req.url == '/') {
                 var d = fs.readFileSync('editor.htm', 'utf-8');
+                unescape(d);
                 res.write(d);
             }
             else if (req.url == '/favicon.ico') {
@@ -25,33 +26,23 @@ http.createServer(function (req, res) {
                 res.write(d);
             }
         }
-        else{
+        else {
             if (req.url == '/launch') {
-                req.on('data', (c) => {
-                    var abuff = _base64ToArrayBuffer(c.toString());
-                    fs.writeFileSync('bin/postflash.bin',abuff);
+                req.on('data', (chunk) => {
+                    data.push(chunk);
+                }).on('end', () => {
+                    var u8a = new Uint8Array(Buffer.concat(data));
+                    data = [];
+                    console.log(u8a);
                 });
+
             }
         }
     } catch (err) {
-        console.log('error');
+        console.log(err);
         res.write('error');
     }
     res.end();
 
 }).listen(8080);
 
-
-function _base64ToArrayBuffer(base64) {
-    var binary_string = atob(base64);
-    var len = binary_string.length;
-    var bytes = new Uint8Array(len);
-    for (var i = 0; i < len; i++) {
-        bytes[i] = binary_string.charCodeAt(i);
-    }
-    return bytes;
-}
-
-function buf2hex(buffer) { // buffer is an ArrayBuffer
-    return Array.prototype.map.call(new Uint8Array(buffer), x => ('00' + x.toString(16)).slice(-2)).join('');
-  }
